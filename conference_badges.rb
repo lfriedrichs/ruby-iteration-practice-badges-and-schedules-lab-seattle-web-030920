4aab14f668b70391644aba4741038de58df75bd7
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-29 02:35:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name) 
-  "Hello, my name is #{name}."
+def batch_badge_creator(names) 
+  output = []
+  names.each {|name| output << badge_maker(name)}
 end
 
 def assign_rooms(name) 
